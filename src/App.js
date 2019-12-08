7ea23fa28c3adaa27bda803479698d900d7a9011
Daniel Erazo <derazodevelopment@gmail.com>
2019-12-08 01:01:05 -0800

    inital commit : React Template

---
+++ src/App.js
@@ -0,0 +1,56 @@
+import React, { Component } from 'react';
+import './App.css';
+
+
+class App extends Component {
+
+
+  state = {
+
+  
+  
+  }
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+    render() {
+  
+      
+  
+  
+  
+  
+  
+  
+  
+  
+        
+  
+  
+  
+  
+  
+  
+  
+  
+      return (
+        <div className="App">
+         
+  
+  
+  
+  
+        </div>
+      );
+    }
+  }
+  
+  export default App;
